1924ed7602a5c299901994f5c0ee30f8459b4e7a
Uttam P <panasalauttam@gmail.com>
2018-02-23 06:41:46 -0500

    Create a Angular Component

---
+++ 1.Installation/hello-world/src/app/courses.component.ts
@@ -0,0 +1,14 @@
+// To create a component
+// Make a file like this
+// Add meta data
+import {Component} from '@angular/core'
+// Create this Class
+
+// Declarator function
+@Component({
+    selector:'courses',  //  <courses>
+    template:'<h2>Courses</h2>'
+})
+export class CoursesComponent{
+
+}
