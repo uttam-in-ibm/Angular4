dedc63e51021f1b4232ec4febd9e2077bfa2b3f5
Uttam P <panasalauttam@gmail.com>
2018-06-01 13:10:55 -0400

    Automatic service Generation

---
+++ 1.Installation/hello-world/src/app/email.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()  // Make it injectable
+export class EmailService {
+
+  constructor() { }
+
+}
+
+/*
+creating a service
+automatically
+ng g s email
+*/
