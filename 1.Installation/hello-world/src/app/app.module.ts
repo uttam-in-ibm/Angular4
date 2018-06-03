a5f36161c044065d2cd43b1641483dfe5f66e84f
Uttam P <panasalauttam@gmail.com>
2018-06-03 05:06:59 -0400

    Custom Pipes

--- 1.Installation/hello-world/src/app/app.module.ts
+++ 1.Installation/hello-world/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
 import { CourseComponent } from './course/course.component';
 import { CoursesService } from './courses.service';
+import { SummaryPipe } from './summary.pipe';
 
 // Add your new component here in declarations
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
-    CourseComponent 
+    CourseComponent,
+    SummaryPipe
   ],
   imports: [
     BrowserModule,
--- 1.Installation/hello-world/src/app/courses.component.ts
+++ 1.Installation/hello-world/src/app/courses.component.ts
@@ -5,20 +5,12 @@ import { CoursesService } from './courses.service';
 @Component({
     selector:'courses',
     template:`    
-        {{courses.title | uppercase | lowercase}} <br/>
-        {{courses.students | number }} <br/>
-        {{courses.rating | number:'2.1-1'}} <br/>
-        {{courses.price | currency:'AUD':true:'3.2-2'}} <br/>
-        {{courses.releaseDate | date:'shortDate'}} <br/>
+       {{text | summary:10}}
     `  
 })
 export class CoursesComponent{
-    courses = {
-        title : 'The title',
-        students : 4.9745,
-        rating: 30123,
-        price: 123456,
-        releaseDate: new Date(2016,3,1)
-    }
+    text = `
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    `
 }
 
